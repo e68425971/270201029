0575acd7b29257b46b1c966e42a3801bc3c63144
e68425971 <ncrsecond@hotmail.com>
2020-11-23 10:14:25 +0000

    5

--- lab5/example5.py
+++ lab5/example5.py
@@ -2,13 +2,13 @@ value = int(input("Write a value: "))
 lister = []
 
 for index in range(0, value):
-    lister.append([])
+  lister.append([])
 
 counter = 0
 for row in lister:
-    for index in range(0, value):
-        row.append(0)
-    row[counter] = 1
-    counter += 1
+  for index in range(0, value):
+    row.append(0)
+  row[counter] = 1
+  counter += 1
 
 print(lister)
