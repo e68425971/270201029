06a3386569ef40025fcb4d61b1dc844930dff97b
e68425971 <ncrsecond@hotmail.com>
2021-01-12 18:00:34 +0000

    sadas

--- lab10/example2.py
+++ lab10/example2.py
@@ -1,37 +1,67 @@
 class Employee:
-    def __init__(self, name, salary):
-        self.salary = salary
-        self.name = name
+    def __init__(self, name, salary):  # Bunu atamadan set veya get de çalışmıyor
+        if (type(name) is str) and (type(salary) is int):
+            self.name = name
+            self.salary = salary
+        else:
+            print("Employee didn't assigned. Please try again after checked")
 
-    def Display(self):
-        self.information = f"Name : {self.name} \nSalary : {self.salary} "
-        return self.information
+    def Salary(self, salary):
+        if salary >= 0:
+            self.salary = salary
+        else:
+            print("Salary can't be less than zero try again")
 
+    def Name(self, name):
+        if type(name) is str:
+            self.name = name
+        else:
+            print("Name have to be string try again")
 
-employee1 = Employee("efe", 4000)
-print(employee1.Display())
+    def SalaryGet(self):
+        return self.salary
+
+    def NameGet(self):
+        return self.name
+
+    def Information(self):
+        self.information = f"Name: {self.name}\n" \
+                           f"Salary: {self.salary}"
+        print(self.information)
 
 
 class Company:
     def __init__(self, employeeList):
-        self.employeeList = list(employeeList)
+        self.employeeList = employeeList
 
-    def newEmployee(self, name, salary):
-        self.additionEmployee = name
-        self.additionSalary = salary
-        self.employeeList += [[self.additionEmployee, self.additionSalary]]
+    def AddEmployee(self, newEmployee):
+        if isinstance(newEmployee, Employee):
+            newEmployeeAdd = [newEmployee.NameGet(), newEmployee.SalaryGet()]
+            self.employeeList += [newEmployeeAdd]
+        else:
+            print("New employee didn't assigned, try again")
 
-    def AverageSalary(self):
+    def Average(self):
         total = 0
-        for salary in self.employeeList:
-            total += salary[1]
-        average = total / len(self.employeeList)
-        self.averageS = average
-        return average
+        for innerLists in self.employeeList:
+            total += innerLists[1]
+        print(round(total / len(self.employeeList)))
+
+    def Display(self):
+        for innerlist in self.employeeList:
+            print(f"Name:{str(innerlist[0]).title()} Salary: {innerlist[1]}")
+
+    def EmployeeListGet(self):
+        return self.employeeList
+
+    def EmployeeListSet(self,newList):
+        if type(newList) is list:
+            self.employeeList = newList
 
 
-object1 = Company([["mert",7000],["genç",4000],["efe",8000]])
-
-print(object1.AverageSalary())
-object1.newEmployee("cenk",7000)
-print(object1.AverageSalary())
+list = [["gaye", 4000], ["cansu", 3000], ["egemen", 8000]]
+NcrTech = Company(list)
+Stajyer = Employee("dido", 2300)
+NcrTech.AddEmployee(Stajyer)
+NcrTech.Display()
+NcrTech.Average()
